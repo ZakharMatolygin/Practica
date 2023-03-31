1ea0b77ca5b670c38cdd8c0593102262ce75e9b8
ZakharMatolygin <125597384+ZakharMatolygin@users.noreply.github.com>
2023-03-31 19:35:57 +0300

    Delete Practica1.8 directory

--- Practica1.8/src/PersonComparatorByFullName.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class PersonComparatorByFullName implements Comparator<Person> {
-
-    @Override
-    public int compare(Person o1, Person o2) {
-        return o1.getFullName().compareTo(o2.getFullName());
-    }
-}
--- Practica1.8/src/PersonComparatorByNumbers.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class PersonComparatorByNumbers implements Comparator<Person> {
-
-    @Override
-    public int compare(Person o1, Person o2) {
-        return o1.getNumber() - o2.getNumber();
-    }
-}
