f662233c4026a422e9dce251359590c40ba1dcf7
ZakharMatolygin <125597384+ZakharMatolygin@users.noreply.github.com>
2023-04-07 19:19:17 +0300

    Add files via upload

---
+++ src/Practica1_14/ThreadPrinter.java
@@ -0,0 +1,38 @@
+package Practica1_14;
+
+public class ThreadPrinter implements Runnable {
+
+    private String threadName;
+    private Object lock;
+
+    public ThreadPrinter(String name, Object lock) {
+        this.threadName = name;
+        this.lock = lock;
+    }
+
+    public void run() {
+        while (true) {
+            synchronized (lock) {
+                System.out.println(threadName);
+                lock.notify();
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(" (Practical №1.14) Variant 4. Group RIBO-03-21. Matolygin Z.I. ");
+        Object lock = new Object();
+        Thread t1 = new Thread(new ThreadPrinter("Thread-0", lock));
+        Thread t2 = new Thread(new ThreadPrinter("Thread-1", lock));
+        t1.start();
+        t2.start();
+        Thread.sleep(5000); // Работает 5 секунд
+        t1.interrupt();
+        t2.interrupt();
+    }
+}
